fe84b241495ca13bffbe99cd1480b58ef4dab20c
nm-woodward <nowoodward15@gmail.com>
2022-06-22 10:33:14 -0700

    Finished asset management tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Flower from './flower.jpeg';
+import Data from './savant_data.csv';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // Adding flower image
+    const myFlower = new Image();
+    myFlower.src = Flower;
+
+    element.appendChild(myFlower);
+
+    console.log(Data[1]);
   
     return element;
   }
